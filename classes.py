3ae0389d9cffcdd04f01c82a07c5a16b9fe09578
Xavier Lee <xavierr@umich.edu>
2025-07-29 20:44:20 -0700

    started classes file

---
+++ classes.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 29 20:20:36 2025
+
+@author: xavierlee
+"""
+
+class card:
+    def __init__(self, suit, rank, value):
+        self.suit = suit
+        self.rank = rank
+        self.value= value
+        
+class deck:
+    def __init__(self, cards):
+        self.cards = cards
+    def shuffle():
+        #randomize cards in deck
+    def deal():
+        #take top card off deck
+
+class hand:
+    def __init__(self):
+        
+    def add_card():
+        
+    def calculate_value():
+    
+    def show_hand():
+        
+class player:
+    def __init__(self):
+        
+        
+    def hit():
+        
+    def stand():
+        
+    
+        
+        
