60865588dfdb09f66845c33365c5971792cb4ecd
Xavier Lee <xavierr@umich.edu>
2025-07-29 21:08:25 -0700

    started game file and wrote some methods

--- classes.py
+++ classes.py
@@ -6,37 +6,53 @@ Created on Tue Jul 29 20:20:36 2025
 @author: xavierlee
 """
 
-class card:
+class Card:
     def __init__(self, suit, rank, value):
         self.suit = suit
         self.rank = rank
         self.value= value
         
-class deck:
-    def __init__(self, cards):
-        self.cards = cards
+class Deck:
+    import random
+    
+    def __init__(self):
+        suits = ['h', 's', 'c', 'd']
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+        values = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':10, 'Q':10, 'K':10, 'A':11}
+    
+        self.order = [Card(suit, rank, value[rank]) for suit in suits for rank in ranks]
+    
     def shuffle():
         #randomize cards in deck
+        self.order = random.shuffle(self.order)
     def deal():
         #take top card off deck
+        
 
-class hand:
+class Hand:
     def __init__(self):
         
     def add_card():
         
-    def calculate_value():
+    def calculate_value(hole_cards):
     
     def show_hand():
         
-class player:
-    def __init__(self):
-        
+class Player:
+    def __init__(self, stack):
+        self.stack = stack
         
     def hit():
-        
+        hand.add_card()
+        hand.calculate_value()
     def stand():
         
     
+class Game:
+    def __init__(self, bet):
+        self.bet = bet
         
+    def start_game():
+        deck.shuffle()
+        deck.deal()
         
---
+++ game.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 29 20:44:58 2025
+
+@author: xavierlee
+"""
+
+#start game
+#ask how much to buy in for
+#ask how much to bet
+#shuffle and deal
+#player has X and dealer shows Y
+#calculate value
+#hit until stand
+#show cards and calculate winner
+#calculate payout and update chip count
+#ask to play again and how much to bet 
